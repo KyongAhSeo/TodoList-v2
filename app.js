4dd609d3c77ac4063de3d1fa7f650600a46d4dc5
DESKTOP-T0IK3GO\sluer <kyongahjulieseo@gmail.com>
2021-03-29 14:49:26 +0900

    commit

--- app.js
+++ app.js
@@ -9,11 +9,16 @@ const _ = require("lodash");
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 //2. 연결
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://kyong-admin:12345@cluster0.x6hee.mongodb.net/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 //3. schema 생성
 const itemsSchema = {
@@ -21,7 +26,7 @@ const itemsSchema = {
 };
 
 //4. 모델 생성
-                            //collection의 이름은 단수형태로
+//collection의 이름은 단수형태로
 const Item = mongoose.model("Item", itemsSchema);
 
 //5. default item 생성
@@ -48,40 +53,49 @@ const listSchema = {
 const List = mongoose.model("List", listSchema);
 
 app.get("/", function(req, res) {
+  var today = new Date();
 
-  //8. find all documents
-  //Item collection에 접근한다.
-  //{} = Item collection에 들어가있는 모든 것을 찾으라는 의미
-  //foundItems = 그 모든 것이 저장되는 변수
-  //9. newListItems에 foundItems이 들어가도록 수정
-  Item.find({}, function(err, foundItems){
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
 
-    //foundItems에 데이터가 들어가 있는지 먼저 체크
-    if(foundItems.length === 0){
-      //7. 아무것도 없으면 데이터 삽입
-      Item.insertMany(defaultItems, function(err){
-        if(err){
+  var day = today.toLocaleDateString("ko-KR", options);
+
+  Item.find({}, function(err, foundItems) {
+
+    if (foundItems.length === 0) {
+
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
           console.log(err);
-        }else{
+        } else {
           console.log("Successfully saved default items to DB.");
         }
       });
-      //루트 라우터로 다시 이동
+
       res.redirect("/");
-      //foundItems에 데이터가 있으면
-    } else{
-      res.render("list", {listTitle: "Today", newListItems: foundItems});
+
+    } else {
+      res.render("list", {
+        kindOfDay: day,
+        listTitle: "Today",
+        newListItems: foundItems
+      });
     }
   });
 });
 
-app.get("/:customListName", function(req, res){
+app.get("/:customListName", function(req, res) {
   const customListName = _.capitalize(req.params.customListName);
 
-  List.findOne({name:customListName}, function(err, foundList){
-    if(!err){
-      if(!foundList){
-        //Create a new List
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+
         const list = new List({
           name: customListName,
           items: defaultItems
@@ -90,15 +104,16 @@ app.get("/:customListName", function(req, res){
         list.save();
         res.redirect("/" + customListName);
       } else {
-        //Show an existing List
-
-        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
       }
     }
   });
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
 
   const itemName = req.body.newItem;
   const listName = req.body.list;
@@ -107,11 +122,13 @@ app.post("/", function(req, res){
     name: itemName
   });
 
-  if (listName === "Today"){
+  if (listName === "Today") {
     item.save();
     res.redirect("/");
-  }else{
-    List.findOne({name:listName}, function(err, foundList){
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
       foundList.items.push(item);
       foundList.save();
       res.redirect("/" + listName);
@@ -119,30 +136,33 @@ app.post("/", function(req, res){
   }
 });
 
-app.post("/delete", function(req, res){
+app.post("/delete", function(req, res) {
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
 
-  if(listName === "Today"){
-    Item.findByIdAndRemove(checkedItemId, function(err){
-      if(!err){
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err) {
+      if (!err) {
         console.log("Successfully deleted the checked item.");
         res.redirect("/");
       };
     });
-  }else{
-    //먼저 List 컬렉션에서 findOneAndUpdate()을 이용하여 데이터를 찾아 삭제한다.
-    //첫번째 인자에는 찾고자하는 list의 이름
-    //두번째 인자에는 업데이트하고자 하는게 들어가는데 $pull을 사용하여 checkedItemId의 아이템을 items array에서 찾아 지울것이다.
-
-    List.findOneAndUpdate({name:listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
-      if(!err){
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
         res.redirect("/" + listName);
       }
-  });
-}
+    });
+  }
 });
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
